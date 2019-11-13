1605670ac778759a66dbe458a780397f57f63ba2
Reza Tavasoli <jfk998@gmail.com>
2019-11-13 21:35:10 +0330

    Exchange new physical key before closing the connection.

--- Client/TCP-Client.py
+++ Client/TCP-Client.py
@@ -2,7 +2,6 @@ import socket
 import file_encrypt as fien
 import os
 from Crypto.Cipher import AES
-import threading
 import datetime
 
 data = ''
@@ -12,17 +11,13 @@ s_time = datetime.datetime.now()
 f_time = datetime.datetime.now()
 
 host_ip, server_port = "127.0.0.1", 8031
-# data = " Hello how are you?\n"
 
-# Initialize a TCP client socket using SOCK_STREAM
 tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, )
 
 
 def create_new_session_key(pswd):
-    # pswd = input("Enter physical key's password: ")
     new_key = input("Enter new session key: ")
     new_key = new_key.encode().rjust(32)
-    # print(len(new_key))
     fien.decrypt(fien.getKey(pswd), 'encrypted_initial_key.txt')
     initial_file = open('initial_key.txt', 'r')
     key_data = initial_file.read().rjust(32)
@@ -36,8 +31,6 @@ def expire_session_key():
     print("Session key has expired.")
 
 
-# timer = threading.Timer(2.0, gfg)
-# timer.start()
 def key_exchange(p_key):
     encrypted_session_key, original_session_key = create_new_session_key(p_key)
     original_session_key = original_session_key.decode().strip()
@@ -64,7 +57,7 @@ def decrypt_physical_key():
 
 def exchange_new_key(pre_session_key):
     sent_data = b'send nkey'
-    print('Session key expired')
+    print('## Session key expired ##')
     new_key = input("Enter new session key: ")
     new_key = new_key.encode().rjust(32)
     cipher = AES.new(fien.getKey(pre_session_key), AES.MODE_ECB).encrypt(new_key)
@@ -72,14 +65,9 @@ def exchange_new_key(pre_session_key):
     return sent_data, new_key.decode().strip()
 
 
-# timer = threading.Timer(20.0, expire_session_key)
 try:
     physical_key = decrypt_physical_key()
     s_time = datetime.datetime.now()
-    # timer = threading.Timer(10.0, gfg)
-    # timer.start()
-
-    # Establish connection to TCP server and exchange data
     tcp_client.connect((host_ip, server_port))
     enc_session_key, orig_session_key = '', ''
 
@@ -87,45 +75,35 @@ try:
         data = ''
         f_time = datetime.datetime.now()
         if (f_time - s_time).total_seconds() > 15:
-            print('yes')
             data, orig_session_key = exchange_new_key(orig_session_key)
             s_time = datetime.datetime.now()
         else:
             print('----------+----------+----------')
-            choose_action = input('Type \'n\' to start new session:\nType \'d\' to send data:\n')
+            choose_action = input('Type \'n\' to start new session, \'d\' to send data, \'e\' to end connection.\n')
+            if choose_action == 'e':
+                raise ConnectionError
             if choose_action == 'n':
                 data, orig_session_key = key_exchange(physical_key)
             elif choose_action == 'd':
                 data = data_exchange(orig_session_key)
 
-        # print('----------+----------+----------')
-        # choose_action = input('Type \'n\' to start new session:\nType \'d\' to send data:\n')
-
-        # if choose_action == 'n':
-        #     # enc_session_key, orig_session_key = create_new_session_key()
-        #     # orig_session_key = orig_session_key.decode().strip()
-        #     # data = b'send keys' + enc_session_key
-        #     data, orig_session_key = key_exchange(physical_key)
-        #
-        # elif choose_action == 'd':
-        #     data = data_exchange(orig_session_key)
-        #     # data = b'send data'
-        #     # file_name = input('Enter filename:\n')
-        #     # # print('this is session key', orig_session_key)
-        #     # fien.encrypt(fien.getKey(orig_session_key), file_name)
-        #     # enc_file_name = 'encrypted_' + file_name
-        #     # f = open(enc_file_name, 'rb')
-        #     # # print(f.read())
-        #     # encrypted_file = f.read()
-        #     # f.close()
-        #     # data += encrypted_file
-        # print('this is data:    ', data)
         print(data)
         tcp_client.sendall(data)
 
         received = tcp_client.recv(1024)
-        # print("Bytes Sent:     {}".format(data))
         print("Bytes Received: {}".format(received.decode()))
+except ConnectionError:
+    inp = input('Enter new physical key')
+    f = open('initial_key.txt', 'w')
+    f.write(inp)
+    f.close()
+    pswd = input("Enter physical key's password: ")
+    fien.encrypt(fien.getKey(pswd), 'initial_key.txt')
+    # os.remove('initial_key.txt')
+    cipher = AES.new(fien.getKey(orig_session_key), AES.MODE_ECB).encrypt(inp.rjust(32))
+    new_data = b'send phys' + cipher
+    tcp_client.send(new_data)
+    print('connection closed!!!')
 
 finally:
     tcp_client.close()
--- Server/TCP-Server.py
+++ Server/TCP-Server.py
@@ -15,35 +15,24 @@ def read_physical_key():
     return key_data
 
 
-class Handler_TCPServer(socketserver.BaseRequestHandler):
+class ServerHandler(socketserver.BaseRequestHandler):
     session_key = ''
     counter = 0
     counter2 = 0
     enc_data = b''
     new_data = b''
     action = ''
-    """
-    The TCP Server class for demonstration.
-
-    Note: We need to implement the Handle method to exchange data
-    with TCP client.
-
-    """
 
     def handle(self):
 
         while True:
             self.data = b''
             self.new_data = b''
-            # self.request - TCP socket connected to the client
             self.data = self.request.recv(1024)
-            # print(self.data)
-            # print('first counter: ', self.counter2)
             try:
                 self.action = self.data[:9].decode()
                 if not (self.action.startswith('send')):
                     raise ValueError
-                # print('how many times i came here')
                 self.new_data = self.data[9:]
                 self.enc_data = b''
                 c_time = ' ' + str(dt.datetime.now()).split('.')[0]
@@ -52,19 +41,14 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
 
             finally:
                 if self.action == 'send keys':
-                    # self.data = self.data[9:]
-                    # print(len(self.data))
                     decipher = AES.new(read_physical_key().rjust(32), AES.MODE_ECB).decrypt(self.new_data)
                     self.session_key = decipher.decode().strip()
-                    # print(self.session_key, '112222233333333333')
-                    # print("{} sent:".format(self.client_address[0]))
                 elif self.action == 'send nkey':
                     print('#important#', self.new_data)
                     decipher = AES.new(fien.getKey(self.session_key), AES.MODE_ECB).decrypt(self.new_data)
                     self.session_key = decipher.decode().strip()
-                    print('##server session key##'+self.session_key)
+                    print('##server session key##' + self.session_key)
                 elif self.action == 'send data':
-                    # print('second counter: ', self.counter)
                     self.enc_data += self.new_data
                     f = open('received__file' + c_time, 'wb')
                     f.write(self.enc_data)
@@ -74,25 +58,26 @@ class Handler_TCPServer(socketserver.BaseRequestHandler):
                         os.remove('received__file' + c_time)
                     except Exception as e:
                         os.remove('received__file' + c_time)
+                elif self.action == 'send phys':
+                    decipher = AES.new(fien.getKey(self.session_key), AES.MODE_ECB).decrypt(self.new_data)
+                    decipher = decipher.decode().strip()
+                    new_file = open('initial_key.txt', 'w')
+                    new_file.write(decipher)
+                    new_file.close()
+                    pswd = input("Enter physical key's password: ")
+                    fien.encrypt(fien.getKey(pswd), 'initial_key.txt')
+                    # os.remove('initial_key.txt')
+                    print('new physical key is', decipher)
 
-                    # self.data = self.data[9:]
-                    # f = open('received_file', 'ab')
-                    # f.write(self.data)
-                    # print(self.data)
-                    # f.close()
-                    # print('this is session key', self.session_key)
-                    # fien.decrypt(fien.getKey(self.session_key), 'received_file')
-
-            self.request.sendall("ACK from TCP Server".encode())
-            # print('encoded data is', self.enc_data)
+            try:
+                self.request.sendall("ACK from TCP Server".encode())
+            except OSError:
+                print('connection not found')
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8031
 
-    # Init the TCP server object, bind it to the localhost on 9999 port
-    tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
+    tcp_server = socketserver.TCPServer((HOST, PORT), ServerHandler)
 
-    # Activate the TCP server.
-    # To abort the TCP server, press Ctrl-C.
     tcp_server.serve_forever()
